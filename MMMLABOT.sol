33fb1673de695ee09642f9019c5882d110e56b93
timeandmoneyceo <97771603+timeandmoneyceo@users.noreply.github.com>
2023-11-19 22:08:40 -0800

    Update MMMLABOT.sol
    matrixMultiply rearanged.

--- MMMLABOT.sol
+++ MMMLABOT.sol
@@ -52,6 +52,23 @@ contract MEVFlashBot is ChainlinkClient {
         fee = 0.1 * 10 ** 18; // 0.1 LINK
     }
 
+    // Function to perform matrix multiplication
+    function matrixMultiply(uint[][] calldata matrixA, uint[][] calldata matrixB) internal pure returns (uint[][] memory result) {
+        require(matrixA[0].length == matrixB.length, "Incompatible matrices");
+
+        result = new uint[][](matrixA.length);
+        for (uint i = 0; i < matrixA.length; i++) {
+            result[i] = new uint[](matrixB[0].length);
+            for (uint j = 0; j < matrixB[0].length; j++) {
+                uint sum = 0;
+                for (uint k = 0; k < matrixA[0].length; k++) {
+                    sum += matrixA[i][k] * matrixB[k][j];
+                }
+                result[i][j] = sum;
+            }
+        }
+    }
+
     // Function to perform matrix multiplication and set prediction
     function performMatrixMultiplicationAndSetPrediction(uint[][] calldata matrixA, uint[][] calldata matrixB) external onlyOwner {
         // Perform matrix multiplication
@@ -74,20 +91,6 @@ contract MEVFlashBot is ChainlinkClient {
         emit MLPredictionReceived("parameter", prediction);
     }
 
-    // Function to execute MEV strategy using the existing prediction
-    function executeMEVStrategyWithExistingPrediction() external onlyOwner {
-        require(prediction.length > 0, "No prediction available");
-
-        uint optimalProfit = 0;
-        address[] memory optimalPath;
-
-        // Rest of the existing code for executing the MEV strategy...
-        // ...
-
-        // Example call to execute MEV strategy with Uniswap
-        // executeMEVStrategyWithUniswap(prediction[0][2], prediction[0][3], optimalPath, block.timestamp + 15 minutes);
-    }
-
     // Function to execute MEV strategy with Uniswap interaction
     function executeMEVStrategyWithUniswap(uint amountIn, uint amountOutMin, address[] calldata path, uint deadline) internal onlyOwner {
         require(path.length >= 2, "Invalid path");
@@ -104,21 +107,18 @@ contract MEVFlashBot is ChainlinkClient {
         emit UniswapTradeExecuted(amounts);
     }
 
-    // Function to perform matrix multiplication
-    function matrixMultiply(uint[][] calldata matrixA, uint[][] calldata matrixB) external pure returns (uint[][] memory result) {
-        require(matrixA[0].length == matrixB.length, "Incompatible matrices");
+    // Function to execute MEV strategy using the existing prediction
+    function executeMEVStrategyWithExistingPrediction() external onlyOwner {
+        require(prediction.length > 0, "No prediction available");
 
-        result = new uint[][](matrixA.length);
-        for (uint i = 0; i < matrixA.length; i++) {
-            result[i] = new uint[](matrixB[0].length);
-            for (uint j = 0; j < matrixB[0].length; j++) {
-                uint sum = 0;
-                for (uint k = 0; k < matrixA[0].length; k++) {
-                    sum += matrixA[i][k] * matrixB[k][j];
-                }
-                result[i][j] = sum;
-            }
-        }
+        uint optimalProfit = 0;
+        address[] memory optimalPath;
+
+        // Rest of the existing code for executing the MEV strategy...
+        // ...
+
+        // Example call to execute MEV strategy with Uniswap
+        executeMEVStrategyWithUniswap(prediction[0][2], prediction[0][3], optimalPath, block.timestamp + 15 minutes);
     }
 
     // Function to set the profit matrix for a strategy
@@ -132,7 +132,8 @@ contract MEVFlashBot is ChainlinkClient {
     }
 
     // Fallback function to receive Ether
-    receive() external payable {
+    receive()
+   external payable {
         if (msg.value > 0) {
             emit ReceivedEther(msg.sender, msg.value);
         }
